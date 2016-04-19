64578c8d227de900086bb9508ada4905a825840c
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-18 21:06:18 -0400

    setup for heroku

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+	if (req.headers['x-forward-proto'] === 'http'){
+		next();
+	}else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
 
 app.use(express.static('public'));
+
 
 
 app.listen(PORT, function(){
