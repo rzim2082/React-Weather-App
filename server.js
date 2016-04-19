304a845765ba359ba61cc14ac57889efc35df121
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-18 21:25:21 -0400

    working out bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-/*
+
 app.use(function(req, res, next){
 	if (req.headers['x-forward-proto'] === 'http'){
 		next();
@@ -10,7 +10,7 @@ app.use(function(req, res, next){
 		res.redirect('http://' + req.hostname + req.url);
 	}
 });
-*/
+
 
 app.use(express.static('public'));
 
