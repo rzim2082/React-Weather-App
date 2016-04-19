3b307ee686e49d5798b3919ea4ee9f92b1a1eb7c
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-19 10:49:15 -0400

    styled about page and links to resources

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 
+
 /*
 var About = React.createClass({
 	render: function(){
@@ -13,8 +14,34 @@ var About = React.createClass({
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the About page!</p>
+			<h1 className="text-center">About</h1>
+			<h4>This project is part of my web development portfolio using the following technologies:</h4>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react/" target="_blank">React</a>
+				</li>
+				<li>
+					<a href="http://foundation.zurb.com/" target="_blank">Foundation</a>
+				</li>
+				<li>
+					<a href="http://expressjs.com/" target="_blank">Express</a>
+				</li>
+				<li>
+					<a href="https://nodejs.org/en/" target="_blank">Node</a>
+				</li>
+				<li>
+					<a href="https://babeljs.io/" target="_blank">ES-6 with Babel</a>
+				</li>
+				<li>
+					<a href="https://github.com/mzabriskie/axios" target="_blank">Axios</a>
+				</li>
+				<li>
+					<a href="http://openweathermap.org/" target="_blank">Open Weather Map API</a>
+				</li>
+				<li>
+					<a href="https://webpack.github.io/" target="_blank">Webpack</a>
+				</li>
+			</ul>
 		</div>
 	)
 };
