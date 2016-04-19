2bdf31da92b1a332170ac8628f46ae49a3c2dd1f
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-18 21:16:13 -0400

    no message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
 	if (req.headers['x-forward-proto'] === 'http'){
